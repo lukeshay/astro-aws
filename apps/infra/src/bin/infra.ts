c2ceb767179855a82647de3c7f194dd81dae7487
Luke Shay <shay.luke17@gmail.com>
2022-11-30 13:39:24 -0600

    Removed GitHubOIDC

--- apps/infra/src/bin/infra.ts
+++ apps/infra/src/bin/infra.ts
@@ -1,10 +1,8 @@
 import * as process from "node:process";
 
-import type { Stack } from "aws-cdk-lib";
 import { App, Tags } from "aws-cdk-lib";
 
 import { ENVIRONMENT_PROPS } from "../lib/constants/environments.js";
-import { GitHubOIDCStack } from "../lib/stacks/github-oidc-stack.js";
 import { MonitoringStack } from "../lib/stacks/monitoring-stack.js";
 import { WebsiteStack } from "../lib/stacks/website-stack.js";
 
@@ -19,36 +17,16 @@ const createStackName = (environment: string, stack: string) =>
 	`AstroAWS-${environment}-${stack}-${env.account}-${env.region}`;
 
 Object.entries(ENVIRONMENT_PROPS).forEach(([environment, environmentProps]) => {
-	const stacks: Stack[] = [];
-
 	const monitoringStack = new MonitoringStack(app, createStackName(environment, "Monitoring"), {
 		...environmentProps,
 		env,
 	});
 
-	stacks.push(monitoringStack);
-
-	const websiteStack = new WebsiteStack(app, createStackName(environment, "WebsiteStack"), {
+	new WebsiteStack(app, createStackName(environment, "Website"), {
 		...environmentProps,
 		cloudwatchDashboard: monitoringStack.cloudwatchDashboard,
 		env,
 	});
-
-	stacks.push(websiteStack);
-
-	if (environment === "DEV") {
-		const gitHubOIDCStack = new GitHubOIDCStack(app, createStackName(environment, "GitHubOIDCStack"), {
-			...environmentProps,
-			env,
-		});
-
-		stacks.push(gitHubOIDCStack);
-	}
-
-	// stacks.forEach((stack) => {
-	// 	Tags.of(stack).add("Project", "AstroAWS");
-	// 	Tags.of(stack).add("Environment", environment);
-	// });
 
 	Tags.of(app).add("Project", "AstroAWS");
 	Tags.of(app).add("Environment", environment);
