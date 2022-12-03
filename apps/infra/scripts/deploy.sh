1a1a52398702276e534e7cce338ab6295bd94aef
Luke Shay <shay.luke17@gmail.com>
2022-12-03 11:28:46 -0600

    Removed unneeded fields from the deploy script

--- apps/infra/scripts/deploy.sh
+++ apps/infra/scripts/deploy.sh
@@ -4,12 +4,5 @@ set -e
 
 ENVIRONMENT="${1:-DEV}"
 AWS_ARGS="${@:2}"
-AWS_ACCOUNT="$(aws sts get-caller-identity --query Account --output text ${AWS_ARGS})"
-AWS_REGION="$(aws configure get region ${AWS_ARGS})"
-
-echo "ENVIRONMENT: ${ENVIRONMENT}"
-echo "AWS_ARGS: ${AWS_ARGS}"
-echo "AWS_ACCOUNT: ${AWS_ACCOUNT}"
-echo "AWS_REGION: ${AWS_REGION}"
 
 yarn cdk deploy --require-approval never "AstroAWS-${ENVIRONMENT}-*" ${AWS_ARGS}
