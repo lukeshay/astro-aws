1e801dcbee806be4d26843edad2c30485666180e
Luke Shay <shay.luke17@gmail.com>
2023-09-15 12:12:07 -0500

    fix linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,14 @@ require("@rushstack/eslint-patch/modern-module-resolution")
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
 	extends: ["@lshay/eslint-config"],
+	overrides: [
+		{
+			files: ["*.test.*"],
+			rules: {
+				"@typescript-eslint/no-unsafe-assignment": "off",
+			},
+		},
+	],
 	root: true,
 	rules: {
 		"import/no-useless-path-segments": "off",
