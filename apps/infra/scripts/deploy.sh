49609d3670128992595ffddb6baf72398ec47876
Luke Shay <shay.luke17@gmail.com>
2023-09-21 10:47:00 -0500

    upgraded deps

--- apps/infra/scripts/deploy.sh
+++ apps/infra/scripts/deploy.sh
@@ -5,4 +5,4 @@ set -e
 ENVIRONMENT="${1:-DEV}"
 AWS_ARGS="${@:2}"
 
-yarn cdk deploy --asset-parallelism --concurrency 4 --require-approval never "AstroAWS-${ENVIRONMENT}-*" ${AWS_ARGS}
+pnpm exec -- cdk deploy --asset-parallelism --concurrency 4 --require-approval never "AstroAWS-${ENVIRONMENT}-*" ${AWS_ARGS}
