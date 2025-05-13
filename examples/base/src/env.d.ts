0be475decc4799369a5d58a63acd041ae27ca1a1
Luke Shay <38476395+lukeshay@users.noreply.github.com>
2025-05-13 16:48:02 +0000

    node-22

--- examples/base/src/env.d.ts
+++ examples/base/src/env.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 declare namespace App {
 	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
