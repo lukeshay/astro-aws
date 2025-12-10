dadd2f4c1f8a2f103e72ec60d0039fb1dca60f7c
Luke Shay <shay.luke17@gmail.com>
2025-12-10 13:08:14 -0600

    chore: add rum

--- apps/docs/public/load-cwr.js
+++ apps/docs/public/load-cwr.js
@@ -1,24 +1,16 @@
-;(function (n, i, v, r, s, c, x, z) {
-	x = window.AwsRumClient = { q: [], n: n, i: i, v: v, r: r, c: c }
-	window[n] = function (c, p) {
-		x.q.push({ c: c, p: p })
-	}
-	z = document.createElement("script")
-	z.async = true
-	z.src = s
-	document.head.insertBefore(z, document.head.getElementsByTagName("script")[0])
-})(
-	"cwr",
-	"fbe0adac-0c2a-4780-9bc3-73e032005599",
-	"1.0.0",
-	"us-east-1",
-	"/cwr.js",
+(function(n,i,v,r,s,c,x,z){x=window.AwsRumClient={q:[],n:n,i:i,v:v,r:r,c:c};window[n]=function(c,p){x.q.push({c:c,p:p});};z=document.createElement('script');z.async=true;z.src=s;document.head.insertBefore(z,document.head.getElementsByTagName('script')[0]);})(
+	'cwr',
+	'341397f3-08be-42a7-9f67-b34b7d457199',
+	'1.0.0',
+	'us-west-2',
+	'/cwr.js',
 	{
-		sessionSampleRate: 1,
-		identityPoolId: "us-east-1:a32bcdfa-cf9b-4010-9a9d-790b5d92f52a",
-		endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
-		telemetries: ["performance", "errors", "http"],
-		allowCookies: true,
-		enableXRay: true,
-	},
-)
+		sessionSampleRate: 0.1 ,
+		identityPoolId: "us-west-2:1571621c-f990-4e9c-88e9-dbb067c679a6" ,
+		endpoint: "https://dataplane.rum.us-west-2.amazonaws.com" ,
+		telemetries: ["performance","errors","http"] ,
+		allowCookies: true ,
+		enableXRay: false ,
+		signing: true // If you have a public resource policy and wish to send unsigned requests please set this to false
+	}
+);
